2ba15b21dcee874b5df40573828e6b4845dd4f57
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-15 11:43:54 +0700

    Update broadcast.py

--- handlers/broadcast.py
+++ handlers/broadcast.py
@@ -2,7 +2,7 @@ from pyrogram import Client , filters
 import asyncio
 from pyrogram.types import Message
 from io import BytesIO, StringIO
-from handlers.help import add_command_help
+from modules.help import add_command_help
 
 while 0 < 6:
     _GCAST_BLACKLIST = get(
